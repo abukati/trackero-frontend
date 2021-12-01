52fe9d26999c6081ad35103d4fc1535249b39571
Noa Nissim <87809765+noanissim@users.noreply.github.com>
2021-12-01 15:06:37 +0200

    adding funcs to the service and adding task input

--- src/cmps/group-preview.vue
+++ src/cmps/group-preview.vue
@@ -14,7 +14,20 @@
          <button @click="deleteGroup">Delete Card</button>
       </section>
       <div class="group-tasks" v-for="task in group.tasks" :key="task.id">
-         <router-link class="group task" :to="``">{{ task }}</router-link>
+         <router-link class="group task" :to="``">{{ task.title }}</router-link>
+      </div>
+      <div class="add-task-section">
+         <div @click="toggleInput">+ Add a task</div>
+         <div v-if="isTaskInputOpen" class="task-add">
+            <input
+               type="text"
+               class="add-task-input"
+               v-model="taskInput"
+               placeholder="Enter a title for this task"
+            />
+            <button @click="addTask">Add task</button>
+            <button @click="toggleInput">X</button>
+         </div>
       </div>
    </section>
 </template>
@@ -28,12 +41,34 @@ export default {
    data() {
       return {
          isListOpen: false,
+         isTaskInputOpen: false,
+         taskInput: ''
       }
    },
-   created() {},
+   created() { },
    methods: {
       toggleOptions() {
          this.isListOpen = !this.isListOpen
+      },
+      toggleInput() {
+         this.isTaskInputOpen = !this.isTaskInputOpen
+      },
+      async addTask() {
+         try {
+            const groupId = this.group.id
+            const newTask = this.$store.dispatch('addTask', {
+
+               groupId,
+               title: this.taskInput
+            })
+
+            this.isTaskInputOpen = false
+            this.taskInput = ''
+            if (newTask) showMsg(`task added `)
+            else showMsg(`Yoy are not allowed to add task`, 'danger')
+         } catch (err) {
+            console.log(err)
+         }
       },
       async deleteGroup() {
          try {
@@ -44,7 +79,7 @@ export default {
             })
             this.isListOpen = false
             if (deletedId) showMsg(`group removed ${deletedId}`)
-            else showMsg(`Yoy are not allowed to remove`, 'danger')
+            else showMsg(`Yoy are not allowed to remove group`, 'danger')
          } catch (err) {
             console.log(err)
          }
