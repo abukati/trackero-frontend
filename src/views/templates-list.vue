be8c9c9f2cbcb167de749d92fa9afbe4c59276cc
BaselBoulos <baselboulos@gmail.com>
2021-12-02 16:44:44 +0200

    Working on styling invites/templates page

--- src/views/templates-list.vue
+++ src/views/templates-list.vue
@@ -1,42 +1,58 @@
 <template>
-   <section class="templates-container">
-      <h2>Templates-list</h2>
-      <form @submit.prevent="addBoard">
-         <input type="text" placeholder="Board title" v-model="newBoardTitle" />
-         <button>Create board</button>
-      </form>
-      <ul v-if="boardsIds">
-         <li v-for="boardId in boardsIds" :key="boardId">
-            <router-link :to="`board/${boardId}`">Board </router-link>
-            <button @click="removeBoard(boardId)">Delete</button>
-         </li>
-      </ul>
-   </section>
+	<section class="workspace-container">
+		<!-- <ul class="clean-list" v-if="boardsIds">
+			<li v-for="boardId in boardsIds" :key="boardId">
+				<router-link :to="`board/${boardId}`">Board </router-link>
+				<button @click="removeBoard(boardId)">Delete</button>
+			</li>
+		</ul> -->
+		<aside class="workspace-side-nav">
+			<nav class="workspace-left-sidebar-container">
+				<div>
+					<ul class="clean-list workspace-main-btns">
+						<li>
+							<a href="#"><span>Boards</span></a>
+						</li>
+						<li>
+							<a href="#"><span>Templates</span></a>
+						</li>
+						<li>
+							<a href="#"><span>Home</span></a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</aside>
+		<!-- <form @submit.prevent="addBoard">
+			<input type="text" placeholder="Board title" v-model="newBoardTitle" />
+			<button>Create board</button>
+		</form> -->
+	</section>
 </template>
 
 <script>
 export default {
-   name: 'templates-list',
-   data() {
-      return {
-         newBoardTitle: '',
-      }
-   },
-   created() {
-      this.$store.dispatch({ type: 'loadBoards' })
-   },
-   methods: {
-      removeBoard(boardId) {
-         this.$store.dispatch({ type: 'removeBoard', boardId })
-      },
-      addBoard() {
-         this.$store.dispatch({ type: 'addBoard', boardTitle: this.newBoardTitle })
-      },
-   },
-   computed: {
-      boardsIds() {
-         return this.$store.getters.boardsIds
-      },
-   },
+	name: 'templates-list',
+	data() {
+		return {
+			newBoardTitle: ''
+		}
+	},
+	created() {
+		this.$store.dispatch({ type: 'loadBoards' })
+	},
+	methods: {
+		removeBoard(boardId) {
+			this.$store.dispatch({ type: 'removeBoard', boardId })
+		},
+		addBoard() {
+			this.$store.dispatch({ type: 'addBoard', boardTitle: this.newBoardTitle })
+		}
+	},
+	computed: {
+		boardsIds() {
+			return this.$store.getters.boardsIds
+		}
+	}
 }
 </script>
