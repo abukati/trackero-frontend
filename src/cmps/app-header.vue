b3b96a5982169c58d7295714dbda4705426ca32c
Artiom Bukati <artiomb7@gmail.com>
2021-12-02 10:56:23 +0200

    Added dependencies

--- src/cmps/app-header.vue
+++ src/cmps/app-header.vue
@@ -16,13 +16,20 @@
                      <img src="@/assets/img/bell-regular.svg" alt="notifications center icon">
                   </span>
                </router-link>
-               <router-link class="nav-item" to="/user">user</router-link>
+               <router-link class="nav-item" to="/user">
+                  <avatar username="Guest"></avatar>
+               </router-link>
             </div>
          </div>
       </nav>
    </header>
 </template>
 <script>
+import Avatar from 'vue-avatar'
+
 export default {
+   components: {
+      Avatar
+   }
 }
 </script>
