9be013cf16b52857b0dee4bafc20085ab7a8cb74
Artiom Bukati <artiomb7@gmail.com>
2021-12-03 21:08:28 +0200

    feels like a massive refactoringgggggg

--- src/cmps/group-preview.vue
+++ src/cmps/group-preview.vue
@@ -1,92 +1,59 @@
 <template>
    <section class="group-preview-container">
       <section class="group-header-section">
-         <div class="title-section">
-            <textarea
-               class="group-title-textarea"
-               :draggable="{ isTitleInputOpen }"
-               @focus="titleInputFocus"
-               @keydown.enter.exact.prevent
-               @keyup.enter.exact="updateGroupTitle"
-               @blur="updateGroupTitle"
-               :value="group.title"
-            ></textarea>
-         </div>
-         <div class="title-actions-section">
+         <!-- <div class="group-header-target"></div> -->
+         <textarea class="group-header-title-textarea" :class="{ 'is-editing': isTitleInputOpen }" :draggable="{ isTitleInputOpen }"
+               @focus="titleInputFocus" @keydown.enter.exact.prevent @keyup.enter.exact="updateGroupTitle"
+               @blur="updateGroupTitle" :value="group.title"></textarea>
+         <h2 class="group-header-title-assist">{{ group.title }}</h2>
+         <div class="group-header-options">
             <button @click="toggleOptions">
                <img :src="require(`@/assets/img/option.png`)" />
             </button>
+            <section v-show="isOptionsListOpen" class="list-actions">
+               <h3>List actions</h3>
+               <button @click="toggleOptions">X</button>
+               <button @click="deleteGroup">Delete Card</button>
+            </section>
          </div>
-         <section v-show="isOptionsListOpen" class="list-actions">
-            <h3>List actions</h3>
-            <button @click="toggleOptions">X</button>
-            <button @click="deleteGroup">Delete Card</button>
-         </section>
       </section>
-      <div class="tasks-and-input-section">
-         <draggable
-            class="group-tasks-section"
-            v-model="tasksList"
-            group="group"
-            draggable=".group-task"
-         >
-            <div class="group-task" v-for="task in group.tasks" :key="task.id">
-               <router-link
-                  :to="`/board/${board._id}/${group.id}/${task.id}`"
-                  class="group-task-link"
-               >
-                  {{ task.id }}
-                  <div
-                     class="task-cover"
-                     :style="{ backgroundColor: task.style.bgColor }"
-                  >
-                     (cover)
-                  </div>
-                  <span class="group-task-options"></span>
-                  <div class="group-task-preview">
-                     <!-- <div class="group-task-labels">{{ task.labels }}</div> -->
-                     <div class="group-task-title">{{ task.title }}</div>
-                     <!-- <div class="group-task-badges"></div>
-                  <div class="group-task-members">{{ task.members }}</div> -->
-                  </div>
-               </router-link>
-            </div>
-         </draggable>
-         <div class="add-task-section">
-            <!-- button section -->
-            <div
-               v-if="!isTaskInputOpen"
-               @click="toggleInput"
-               class="add-task-button"
-            >
-               <a href="#" class="card-composer">
-                  <span class="add-task-plus-icon">
-                     <img src="@/assets/img/plus-icon.svg" />
-                  </span>
-                  <span class="add-task-span">Add a card</span>
-               </a>
-            </div>
-
-            <!-- card composer section -->
-            <div v-if="isTaskInputOpen" class="card-composer-open">
-               <div class="add-task-input-section">
-                  <div class="add-task-input-details group-task-link">
-                     <textarea
-                        type="text"
-                        class="add-task-input"
-                        v-model="taskInput"
-                        placeholder="Enter a title for this card..."
-                     />
-                  </div>
+      <draggable class="group-tasks-section" v-model="tasksList" group="group" draggable=".group-task">
+         <div class="group-task" v-for="task in group.tasks" :key="task.id">
+            <router-link :to="`/board/${board._id}/${group.id}/${task.id}`" class="group-task-link">
+               {{ task.id }}
+               <div class="task-cover" :style="{ backgroundColor: task.style.bgColor }">
+                  (cover)
                </div>
-               <div class="add-task-control-section">
-                  <button @click="addTask" class="add-card-confirm-btn">
-                     Add list
-                  </button>
-                  <span @click="toggleInput" class="cancel-add-card">
-                     <img src="@/assets/img/cancel-icon.svg" />
-                  </span>
+               <span class="group-task-options"></span>
+               <div class="group-task-preview">
+                  <div class="group-task-title">{{ task.title }}</div>
                </div>
+            </router-link>
+         </div>
+      </draggable>
+      <div class="task-composer-container">
+         <div v-if="!isTaskInputOpen" @click="toggleInput" class="add-task-button">
+            <a href="#" class="card-composer">
+               <span class="add-task-plus-icon">
+                  <img src="@/assets/img/plus-icon.svg" />
+               </span>
+               <span class="add-task-span">Add a card</span>
+            </a>
+         </div>
+         <div v-else class="card-composer-open">
+            <div class="add-task-input-section">
+               <div class="add-task-input-details group-task-link">
+                  <textarea type="text" class="add-task-input" v-model="taskInput"
+                     placeholder="Enter a title for this card..." />
+               </div>
+            </div>
+            <div class="add-task-control-section">
+               <button @click="addTask" class="add-card-confirm-btn">
+                  Add list
+               </button>
+               <span @click="toggleInput" class="cancel-add-card">
+                  <img src="@/assets/img/cancel-icon.svg" />
+               </span>
             </div>
          </div>
       </div>
