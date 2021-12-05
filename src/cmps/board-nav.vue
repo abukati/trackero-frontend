623683742140f665e1f44289b7e1f389d8ca459c
Artiom Bukati <artiomb7@gmail.com>
2021-12-05 19:42:17 +0200

    board header side menu

--- src/cmps/board-nav.vue
+++ src/cmps/board-nav.vue
@@ -80,7 +80,7 @@
 					</button>
 				</div>
 			</span>
-			<a  class="board-header-btn mod-show-menu show-sidebar">
+			<a @click="toggleBoardNavMenu" class="board-header-btn mod-show-menu show-sidebar">
 				<span class="icon-overflow-menu board-header-btn-icon">
 					<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 488 488" style="enable-background:new 0 0 488 488;" xml:space="preserve"><circle style="fill:currentColor;" cx="51.2" cy="244" r="51.2"/><path style="fill:currentColor;" d="M51.2,295.2C23.2,295.2,0,272,0,244"/><g><path style="fill:currentColor;" d="M102.4,244c0,28-23.2,51.2-51.2,51.2"/><path style="fill:currentColor;" d="M15.2,208c20-20,52-20,72,0s20,52,0,72"/></g><circle style="fill:currentColor;" cx="244" cy="244" r="51.2"/><path style="fill:currentColor;" d="M244,295.2c-28,0-51.2-23.2-51.2-51.2"/><g><path style="fill:currentColor;" d="M295.2,244c0,28-23.2,51.2-51.2,51.2"/><path style="fill:currentColor;" d="M208,208c20-20,52-20,72,0s20,52,0,72"/></g><circle style="fill:currentColor;" cx="436.8" cy="244" r="51.2"/><path style="fill:currentColor;" d="M436.8,295.2c-28,0-51.2-23.2-51.2-51.2"/><g><path style="fill:currentColor;" d="M488,244c0,28-23.2,51.2-51.2,51.2"/><path style="fill:currentColor;" d="M400.8,208c20-20,52-20,72,0s20,52,0,72"/></g></svg>
 				</span>
@@ -125,6 +125,9 @@ export default {
 		},
 		changeBoardBgc(ev) {
 			this.$store.dispatch({ type: 'changeBoardBgc', bgc: ev.target.value })
+		},
+		toggleBoardNavMenu(ev) {
+			this.$emit('toggleBoardNavMenu', ev)
 		},
 		toggleBoardStar() {
 			// TODO: wire up
