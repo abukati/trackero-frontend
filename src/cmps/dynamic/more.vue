f5c50588bc17deacaa825e032420d9828b9f4dcc
Artiom Bukati <artiomb7@gmail.com>
2021-12-11 14:22:45 +0200

    cmp for responsive header

---
+++ src/cmps/dynamic/more.vue
@@ -0,0 +1,40 @@
+<template>
+   <div class="nav-main-links">
+      <a data-cmp="Workspaces" class="nav-link-btn">
+         <span data-cmp="Workspaces" class="btn-text">Workspaces</span>
+         <span data-cmp="Workspaces" class="btn-icon-container">
+            <span data-cmp="Workspaces" class="modal-icon">
+               <svg width="24px" height="24px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M18.5303 9.46967C18.8232 9.76256 18.8232 10.2374 18.5303 10.5303L12.5303 16.5303C12.2374 16.8232 11.7626 16.8232 11.4697 16.5303L5.46967 10.5303C5.17678 10.2374 5.17678 9.76256 5.46967 9.46967C5.76256 9.17678 6.23744 9.17678 6.53033 9.46967L12 14.9393L17.4697 9.46967C17.7626 9.17678 18.2374 9.17678 18.5303 9.46967Z" fill="currentColor" />
+               </svg>
+            </span>
+         </span>
+      </a>
+      <a data-cmp="Starred" class="nav-link-btn">
+         <span data-cmp="Starred" class="btn-text">Starred</span>
+         <span data-cmp="Starred" class="btn-icon-container">
+            <span class="modal-icon">
+               <svg width="24px" height="24px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M18.5303 9.46967C18.8232 9.76256 18.8232 10.2374 18.5303 10.5303L12.5303 16.5303C12.2374 16.8232 11.7626 16.8232 11.4697 16.5303L5.46967 10.5303C5.17678 10.2374 5.17678 9.76256 5.46967 9.46967C5.76256 9.17678 6.23744 9.17678 6.53033 9.46967L12 14.9393L17.4697 9.46967C17.7626 9.17678 18.2374 9.17678 18.5303 9.46967Z" fill="currentColor" />
+               </svg>
+            </span>
+         </span>
+      </a>
+      <a data-cmp="Templates" class="nav-link-btn">
+         <span data-cmp="Templates" class="btn-text">Templates</span>
+         <span data-cmp="Templates" class="btn-icon-container">
+            <span class="modal-icon">
+               <svg width="24px" height="24px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M18.5303 9.46967C18.8232 9.76256 18.8232 10.2374 18.5303 10.5303L12.5303 16.5303C12.2374 16.8232 11.7626 16.8232 11.4697 16.5303L5.46967 10.5303C5.17678 10.2374 5.17678 9.76256 5.46967 9.46967C5.76256 9.17678 6.23744 9.17678 6.53033 9.46967L12 14.9393L17.4697 9.46967C17.7626 9.17678 18.2374 9.17678 18.5303 9.46967Z" fill="currentColor" />
+               </svg>
+            </span>
+         </span>
+      </a>
+   </div>
+</template>
+
+<script>
+   export default {
+      name: 'More'
+   }
+</script>
